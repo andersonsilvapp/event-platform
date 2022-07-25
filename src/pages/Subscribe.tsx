5d4046cdd791177f909b354f3e6f983ca2df8783
Anderson <andersonsilvapp@gmail.com>
2022-07-25 10:50:40 -0300

    fix: alignment

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -28,7 +28,7 @@ export function Subscribe() {
 
   return (
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
+      <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto px-4 flex-col lg:flex-row">
         <div className="max-w-[640px]">
           <Logo />
 
